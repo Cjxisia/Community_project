9c537bae727c65c7697a5beba2a13cab83121a9b
Cjxisia <100449269+Cjxisia@users.noreply.github.com>
2023-11-07 10:24:51 +0900

    Update login_db.js

--- community/config/login_db.js
+++ community/config/login_db.js
@@ -3,7 +3,7 @@ const mysql = require('mysql');
 const connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
-    password: 'ijj2848',
+    password: 'password',
     database:'login'
 });
 
